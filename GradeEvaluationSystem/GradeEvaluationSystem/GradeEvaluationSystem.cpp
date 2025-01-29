3775bffcdb1541dff980ca2dbaf23a8d7dbc8b1d
Karol Suarez <no-reply@example.com>
2025-01-29 11:13:20 -0800

    I used if stamnets to output how the user did  in their exam

---
+++ GradeEvaluationSystem/GradeEvaluationSystem/GradeEvaluationSystem.cpp
@@ -0,0 +1,27 @@
+// take int score, use if else, output fail
+//
+#include <iostream>
+using namespace std; 
+
+int main()
+{
+	int score;
+
+	cout << "Enter your score: ";
+	cin >> score;
+	
+	if (score < 60) {
+		cout << "Fail";
+	}
+	else if (score >= 60 && score < 69) {
+		cout << "Pass";
+	}
+	else if (score > 70 && score < 89) {
+		cout << "Good";
+	}
+	else {
+		cout << "Excellent!";
+	}
+
+	return 0;
+}
--- hdgjklhdfjk/hdgjklhdfjk/hdgjklhdfjk.cpp
+++ hdgjklhdfjk/hdgjklhdfjk/hdgjklhdfjk.cpp
@@ -2,19 +2,47 @@
 //
 
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    double made;
+
+    double commission;
+
+        if (made < 10000) {
+            commission = made * .10;
+        }
+        else if  (made > 10000 && made <= 15000) {
+            commission = made * .15;
+        }
+        else  (made > 15000) {
+            commission = made * .20;
+        }
+
+
+
+       bool usCitizen = true;
+       int tuition;
+       bool caResident = true;
+      
+
+
+       if (usCitizen) {
+           if (caResident) {
+               tuition = 0;
+           }
+           else{
+               tuition = 1000;
+           }
+       }
+       else {
+           tuition = 3000;
+       }
+
+
+
+        return 0;
+
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
