e0ae511d31f421f416efba7a4dd9790c207211da
Karol Suarez <no-reply@example.com>
2025-01-31 16:30:34 -0800

    Created an array

--- CurrencyConversionSystem/CurrencyConversionSystem/CurrencyConversionSystem.cpp
+++ CurrencyConversionSystem/CurrencyConversionSystem/CurrencyConversionSystem.cpp
@@ -2,21 +2,63 @@
 //
 
 #include <iostream>
+#include <iomanip>
+#include <string>
+#include <cctype>
+
 using namespace std;
 
+//conversion rates between USD, EUR, GBP, and JPY
+
+double convertcurrency(double, double);
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	string currencies[]{ "USD","EUR","GBP","JPY" };
+	// current currency rates from USD dolllar 1/29/2025
+	const double EUR = .96;
+	const double GBP = .80;
+	const double JPY = 154.45;
+
+	const double conversionRates[4][4]{// rows = from, colums = to
+		1,EUR,GBP,JPY,
+		(1 / EUR),1,(1 / EUR) * GBP,(1 / EUR) * JPY,// 1/rate converts it into USD 
+		(1 / GBP),(1 / GBP) * EUR,1,(1 / GBP) * JPY,
+		(1 / JPY),(1 / JPY) * EUR,(1 / JPY) * GBP,1
+	};
+
+	string initialCurrency;
+	string toCurrency;
+	double amount;
+	double rate;
+
+
+	cout << left << setw(10) << "*" << "USD, EUR, GBP, and JPY Currency Converter" << right << setw(10) << "*";
+	cout << endl << endl;
+
+
+	cout << "Enter initial currency: ";
+	cin >> initialCurrency;;//need to check, check that the are words
+	cout << "Enter currency you would like to convert to: ";
+	cin >> toCurrency; //need to check
+	cout << "Enter amount: ";
+	cin >> amount;
+
+	int fromIndex = -1, toIndex = -1;//error handeling
+	for (int i = 0; i < 4; i++) {//will loop 4 times 
+		if (currencies[i] == initialCurrency) fromIndex = i;//if"USD" = their input that is the row(from)
+		if (currencies[i] == toCurrency) toIndex = i;
+	}
+	double convertedAmount = convertcurrency(amount, conversionRates[fromIndex][toIndex]);
+	cout << fixed << setprecision(2);
+	cout << amount << " " << initialCurrency << " is " << convertedAmount << " " << toCurrency;
+
+	return 0;
+
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+double convertcurrency(double amount, double rate) {
+	return (amount * rate);
+}
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+
