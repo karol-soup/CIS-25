b9f4ff9b5f28b8f4530dd4654cbfd1b431c7d281
Karol Suarez <no-reply@example.com>
2025-02-01 00:11:30 -0800

    I added comments as well as functions such as setfill and setprecision

--- ManipulatingConsoleOutput/ManipulatingConsoleOutput/ManipulatingConsoleOutput.cpp
+++ ManipulatingConsoleOutput/ManipulatingConsoleOutput/ManipulatingConsoleOutput.cpp
@@ -8,21 +8,30 @@ using namespace std;
 
 int main()
 {
-	int idNumber = 00001;
-	double balance = 73.96;
+	int id = 1;
+	int id2 = 2;
 	string name = "Karol";
-
-	int idNumber2 = 00002;
-	double balance2 = 100.06;
 	string name2 = "Carly";
+	double balance = 100.00;
+	double balance2 = 50.00;
+
+	cout << setw(27) << "Daily Report" << endl;
+	cout << left << setw(10) << "ID" << setw(10) << "Name" << setw(10) << "Balance" << endl; //header //left so ID is to the left not the right
+
+	cout << setfill('-') << setw(5) << "-" << "" << setfill(' ') << setw(5) << "";//"" allows the - to separate
+	cout << setfill('-') << setw(5) << "-" << "" << setfill(' ') << setw(5) << ""; // fills w/ - and to ensure it doesnt to any other white spaces we ' '
+	cout << setfill('-') << setw(5) << "-" << "" << setfill(' ') << setw(5) << "" << endl;
+	
+
+	cout << setfill('0') << setw(5) << right << id << setfill(' ') << ""; //fills with 0 id to right insted of it going first
+	cout << setw(10) << name;
+	cout << setw(7) << fixed << setprecision(2) << "$"  << balance << endl;// 7 because setw didnt account for the 3 char of .00
+
+	cout << setfill('0') << setw(5) << right << id2 << setfill(' ') << "";
+	cout << setw(10) << name2;
+	cout << setw(7) << fixed << setprecision(2) << "$" << balance2 << endl;
 
 	
-		cout << setw(30) << "Daily Report" << endl;//; pushes daily report to the right
-		cout << left << setw(10) << "ID" << setw(10) << "Name" << setw(10) << "Balance" << endl;//pushes output to the left
-		cout << left << setw(10) << "----" << setw(10) << "------" << setw(10) << "------" << endl;// by making setw() the smae we ensure l the output is alligned
-		cout << left << setw(10) << idNumber << setw(10) << name << "$" << balance << endl;//displays output
-		cout << left << setw(10) << idNumber2 << setw(10) << name2 << "$" << balance2 << endl;
-
 		return 0;
 	
 
