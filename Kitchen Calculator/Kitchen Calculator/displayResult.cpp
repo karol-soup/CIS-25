2ba86b590e42fbe84d9fab61e3583e6edd24da3f
Karol Suarez <no-reply@example.com>
2025-02-15 16:46:28 -0800

    I  edited displayResult.cpp. A added conversion: and <<endl;

---
+++ Kitchen Calculator/Kitchen Calculator/displayResult.cpp
@@ -0,0 +1,25 @@
+
+#include <iostream>
+#include <string>
+#include <iomanip>
+using namespace std;
+
+void displayResult(double convertedAmount, double amount, int sUnit, int dUnit) {
+	const int SIZE = 4;
+	string units[SIZE]{ "cups","tbsp","tsp","FL OZ" };
+	string dsUnit;
+	string ddUnit;
+	for (int i = 0; i < SIZE; i++) {
+		if (sUnit == (i + 1)) {
+			dsUnit = units[i];
+		}
+		if (dUnit == (i + 1)) {
+			ddUnit = units[i];
+		}
+	}
+	cout << endl;
+	cout << "Convesion:" << endl;
+	cout << amount << " " << dsUnit << " = " << fixed << setprecision(2) << convertedAmount << " " << ddUnit << endl;
+
+}
+
