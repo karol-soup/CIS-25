cd85d42bb25131015e20df3f61f6a4a32c2184f6
Karol Suarez <no-reply@example.com>
2025-03-11 19:18:04 -0700

    put all my functions in cpp and h files

--- Dynamic Temperature Monitor(vectors)/Dynamic Temperature Monitor(vectors)/Dynamic Temperature Monitor(vectors).cpp
+++ Dynamic Temperature Monitor(vectors)/Dynamic Temperature Monitor(vectors)/Dynamic Temperature Monitor(vectors).cpp
@@ -3,71 +3,32 @@
 
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include "monitorForVectors.h"
 using namespace std;
 
-//                    to pass be refernce we want o modify
-void getTemps(vector<int>& temperatures) {
-	cout << "Enter your tempeture readings:" << endl;
 
-	for (int i = 0; i < temperatures.size(); i++) {
-		while (true) {
-			cout << "Hour " << (i + 1) << endl;
-			cin >> temperatures[i];
-			if (cin.fail()) {
-				cin.clear();
-				cin.ignore(numeric_limits<streamsize>::max(), '\n');
-				cout << "Enter valid input." << endl;
-			}
-
-			if (temperatures[i] < -100 || temperatures[i] > 100) {
-
-				cout << "Must be between -100 - 100" << endl;
-			}
-
-			else break;
-		}
-
-	}
-}
-                    // we dont want to modify anything
-void highesTempRead(const vector<int>& temperatures, int windowSize) {
-	cout << "Highest temperture Readings = [";
-	// <=  to read last element too     bounds checking
-
-	for (int i = 0; i <= temperatures.size() - windowSize; i++) {
-
-		// assume max temp=first i from window
-		int maxTemp = temperatures.at(i);
-		//              checking from that window
-		for (int j = 1; j < windowSize; j++) {
-			if (temperatures.at(j + i) > maxTemp) {
-				maxTemp = temperatures.at(j + i);
-			}
-		}
-		cout << maxTemp;
-		if (i < (temperatures.size() - windowSize)) {
-			cout << ",";
-		}
-	}
-	cout << "]" << endl;
-}
 
 int main() {
-	int size = 0;
-	cout << "enter number of temperture readings: ";
-		cin >> size;
+	int size = manic:: getSize();
+
 	vector<int> temperatures(size);
 
-	getTemps(temperatures);
+	manic:: getTemps(temperatures);
+
+	system("cls");
 
 	cout << "Temperture Readings = ";
 	for (int val : temperatures)
 		cout << val << " ";
 	cout << endl;
 
-	int windowSize = 3;
+	int windowSize = manic::getWindow(size);
 
-	highesTempRead(temperatures, windowSize);
+	manic::highestTempRead(temperatures, windowSize);
+
+	manic::minAndMax(temperatures);
 
 	return 0;
 }
+
