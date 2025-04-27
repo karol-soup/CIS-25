a240a607497631f8bf9d075c9230692513dfd56e
karol-soup <Karol Suarez@DESKTOP-ONS08P7>
2025-04-26 20:20:49 -0700

    pushing modified files

--- Kitchen Calculator/Kitchen Calculator/Kitchen Calculator.cpp
+++ Kitchen Calculator/Kitchen Calculator/Kitchen Calculator.cpp
@@ -1,26 +1,57 @@
 
 #include <iostream>
 #include <iomanip>
-#include "convertUnit.h"
-#include "display.h"
-#include "displayResult.h"
-#include "inputAmount.h"
-#include "inputDesired.h"
-#include "inputStart.h"
-#include "intoFL.h"
+#include "utils/convertUnit.h"
+#include "utils/display.h"
+#include "utils/displayResult.h"
+#include "utils/inputAmount.h"
+#include "utils/inputDesired.h"
+#include "utils/inputStart.h"
+#include "utils/intoFL.h"
 
-using namespace std; 
+#include <string>
+#include <vector>
+using namespace std;
+
+struct Fraction {
+    double numerator = 1;
+    double denominator = 1;// n/1 = n
+};
+
+istream& operator>>(istream& input, Fraction& fraction) {
+    char slash;
+    input >> fraction.numerator >> slash >> fraction.denominator;
+    if (slash != '/') {
+        input.setstate(std::ios::failbit);//exception
+        //
+    }
+    return input;
+}
 
 int main()
 {
-	
+    Fraction myFraction;
+    cout << "Enter your starting amount (e.g., 1/2): ";
+    cin >> myFraction;
+
+    if (cin.fail()) {
+        cout << "Invalid fraction format." << endl;
+    }
+    else {
+        cout << "Numerator: " << myFraction.numerator << endl;
+        cout << "Denominator: " << myFraction.denominator << endl;
+    }
+
+    double fraction = myFraction.numerator / myFraction.denominator;
+    cout << fraction;
 	double amount = 0,
 	convertedAmount = 0;
 	int startUnit = 0, desiredUnit = 0;
 	
 	display();
 
-    amount = inputAmount(amount);
+    double fraction = myFraction.numerator / myFraction.denominator;
+   // amount = inputAmount(amount);
 	startUnit = inputStart(startUnit);
 	desiredUnit = inputDesired(desiredUnit);
 
